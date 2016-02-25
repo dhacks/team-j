b53cfce16bd83c71d9ba9217efcb96a93687d1d0
johshisha <johshisha@gmail.com>
2016-02-26 03:39:15 +0900

    新しいgroup_idに対応

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -6,7 +6,14 @@ def chat
     @messages = Chat.where('man_id = :man_id', man_id: @man_id).where('woman_id = :woman_id', woman_id: @woman_id)
     @man_name = User.find(@man_id).name
     @woman_name = User.find(@woman_id).name
-    @group_id = @messages[0].group_id
+    if @messages[0] == nil then
+        puts("maximum",Chat.maximum('group_id'))
+        @new_id = Chat.maximum('group_id') + 1
+        puts(@new_id)
+        @group_id = @new_id
+    else
+        @group_id = @messages[0].group_id
+    end
     
 end
 
